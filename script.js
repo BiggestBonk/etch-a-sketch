b69028e1a26231e9e4edc2dfb0872a8dea748ef5
biggestbonk <harry.walsh505@gmail.com>
2024-08-30 12:32:28 +1200

    add abilty to draw (16x16 basic)

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-const container = document.querySelector('.container')
-
+const grid = document.querySelector('.grid')
+let isDrawing = false
+const square = document.querySelector('.square')
 function createGrid() {
   for (let i = 0; i < 256; i++) {
     const gridSquare = document.createElement('div')
-    gridSquare.className = 'square'
-    container.appendChild(gridSquare)
+    gridSquare.setAttribute('ondragstart', 'fixDrag(event)')
+    gridSquare.classList.add('square', 'onHover')
+    grid.appendChild(gridSquare)
   }
 }
+document.addEventListener('mouseover', (e) => {
+  if (e.target.matches('.square')) {
+    if (isDrawing === true) {
+      e.target.classList.add('drawnBox')
+    }
+  } else {
+    return
+  }
+})
+
+document.addEventListener('mousedown', (e) => {
+  if (e.target.matches('.square')) {
+    e.target.classList.add('drawnBox')
+  }
+})
 
 createGrid()
 
-document.addEventListener('mouseover', (e) => {
-  if (e.target.matches('.square')) {
-    console.log('hover')
-    e.target.className += ' onHover'
-  }
+grid.addEventListener('mousedown', () => {
+  isDrawing = true
+  console.log('isDrawing')
 })
+
+grid.addEventListener('mouseup', () => {
+  isDrawing = false
+  console.log('notDrawing')
+})
+
+function fixDrag(event) {
+  event.preventDefault()
+  console.log('default prevented')
+}
+//document.addEventListener('mousedown', (e) => {
+//  if (e.target.matches('.square')) {
+//   draw = true
+//    e.target.classList.add('drawnBox')
+// }
+//})
